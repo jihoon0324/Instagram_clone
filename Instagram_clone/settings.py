0a9c7d719497791b0fb2dda918da5e19189ebfe2
Jihoon Oh <jihoon.oh@edu.sait.ca>
2023-01-05 15:48:09 -0700

    done user create

--- Instagram_clone/settings.py
+++ Instagram_clone/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 import environ
 
@@ -133,10 +133,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL ='users.User'
--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import Main
+from .views import Main, UploadPost
 
-
-app_name="posts"
+app_name = "posts"
 urlpatterns = [
 
-    path('', Main.as_view() , name="Main"),
-
+    path('', Main.as_view(), name="Main"),
+    path('upload', UploadPost.as_view()),
 ]
